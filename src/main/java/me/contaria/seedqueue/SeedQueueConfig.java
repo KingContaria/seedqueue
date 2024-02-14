319b902e8e50715c84ee965a157f43beefdc73e7
contaria <kingcontaria@gmail.com>
2024-02-14 12:54:11 +0100

    add some comments to options that are for removal

--- src/main/java/me/contaria/seedqueue/SeedQueueConfig.java
+++ src/main/java/me/contaria/seedqueue/SeedQueueConfig.java
@@ -84,10 +84,12 @@ public class SeedQueueConfig implements SpeedrunConfig {
     @Config.Access(setter = "setChunkUpdateThreads")
     public int chunkUpdateThreads = 0;
 
+    // this option is for removal and should just always be rows x columns
     @Config.Category("performance")
     @Config.Numbers.Whole.Bounds(min = 0, max = 50, enforce = Config.Numbers.EnforceBounds.MIN_ONLY)
     public int previewSetupBuffer = 0;
 
+    // this option is for removal and should just always be 1
     @Config.Category("performance")
     @Config.Numbers.Whole.Bounds(min = 0, max = 50, enforce = Config.Numbers.EnforceBounds.MIN_ONLY)
     public int backgroundPreviewSetupBuffer = 0;
@@ -95,6 +97,8 @@ public class SeedQueueConfig implements SpeedrunConfig {
     @Config.Category("performance")
     public boolean renderPreviewsBeforeBackgroundSetup = true;
 
+    // this option is for removal and should just always be true,
+    // before that I want to implement a system that if no background preview is set up in a frame, it clears one worldrenderer instead
     @Config.Category("performance")
     public boolean lazilyClearWorldRenderers = false;
 
@@ -102,6 +106,8 @@ public class SeedQueueConfig implements SpeedrunConfig {
     @Config.Numbers.Whole.Bounds(min = 0, max = 50, enforce = Config.Numbers.EnforceBounds.MIN_ONLY)
     public int previewRenderLimit = 0;
 
+    // this option might be removed in the future and just always set to true,
+    // it shouldn't hurt performance in any meaningful way and would make the use of previewRenderLimit feel much smoother
     @Config.Category("performance")
     public boolean alwaysRenderChunkMap = false;
 
