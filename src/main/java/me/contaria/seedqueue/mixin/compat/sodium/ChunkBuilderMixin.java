3e9fdc75b6d618feeedb571944b2aff6620a1501
contaria <kingcontaria@gmail.com>
2024-05-16 23:38:24 +0200

    fix: sodium workers staying alive after they're supposed to shut down

--- src/main/java/me/contaria/seedqueue/mixin/compat/sodium/ChunkBuilderMixin.java
+++ src/main/java/me/contaria/seedqueue/mixin/compat/sodium/ChunkBuilderMixin.java
@@ -10,14 +10,25 @@ import me.jellysquid.mods.sodium.client.model.vertex.type.ChunkVertexType;
 import me.jellysquid.mods.sodium.client.render.chunk.compile.ChunkBuildBuffers;
 import me.jellysquid.mods.sodium.client.render.chunk.compile.ChunkBuilder;
 import me.jellysquid.mods.sodium.client.render.chunk.passes.BlockRenderPassManager;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Mixin(value = ChunkBuilder.class, remap = false)
 public abstract class ChunkBuilderMixin {
+
+    @Mutable
+    @Shadow
+    @Final
+    private AtomicBoolean running;
 
     @ModifyReturnValue(
             method = "getOptimalThreadCount",
@@ -53,6 +64,18 @@ public abstract class ChunkBuilderMixin {
     )
     private boolean doNotWaitForWorkersToStopOnWall(Thread thread) {
         return !SeedQueue.isOnWall();
+    }
+
+    // because ChunkBuilderMixin#doNotWaitForWorkersToStopOnWall prevents waiting for old workers to shut down,
+    // it's necessary to replace the atomic boolean to prevent the worker threads staying alive when new workers are started
+    @Inject(
+            method = "stopWorkers",
+            at = @At("TAIL")
+    )
+    private void replaceRunningAtomicBooleanOnWall(CallbackInfo ci) {
+        if (SeedQueue.isOnWall()) {
+            this.running = new AtomicBoolean();
+        }
     }
 
     @WrapOperation(
