7b0442040f07c743ceaf5c14648e8d37a4aa6ee3
contaria <kingcontaria@gmail.com>
2024-08-07 16:12:09 +0200

    change: dont render previous worlds artifacts on wall screen

--- src/main/java/me/contaria/seedqueue/mixin/compat/worldpreview/render/InGameHudMixin.java
+++ src/main/java/me/contaria/seedqueue/mixin/compat/worldpreview/render/InGameHudMixin.java
@@ -1,16 +1,21 @@
 package me.contaria.seedqueue.mixin.compat.worldpreview.render;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import me.contaria.seedqueue.SeedQueue;
+import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.ChatHud;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.gui.hud.SubtitlesHud;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.Text;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(InGameHud.class)
-public abstract class InGameHudMixin {
+public abstract class InGameHudMixin extends DrawableHelper {
 
     @WrapWithCondition(
             method = "render",
@@ -32,5 +37,45 @@ public abstract class InGameHudMixin {
     )
     private boolean doNotRenderChatOnWall(ChatHud chatHud, MatrixStack matrices, int i) {
         return !SeedQueue.isOnWall();
+    }
+
+    @ModifyExpressionValue(
+            method = "render",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lnet/minecraft/client/gui/hud/InGameHud;overlayMessage:Lnet/minecraft/text/Text;",
+                    opcode = Opcodes.GETFIELD,
+                    ordinal = 0
+            )
+    )
+    private Text doNotRenderOverlayMessageOnWall(Text overlayMessage) {
+        if (SeedQueue.isOnWall()) {
+            return null;
+        }
+        return overlayMessage;
+    }
+
+    @ModifyExpressionValue(
+            method = "render",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lnet/minecraft/client/gui/hud/InGameHud;title:Lnet/minecraft/text/Text;",
+                    opcode = Opcodes.GETFIELD,
+                    ordinal = 0
+            )
+    )
+    private Text doNotRenderTitleMessageOnWall(Text title) {
+        if (SeedQueue.isOnWall()) {
+            return null;
+        }
+        return title;
+    }
+
+    @ModifyVariable(
+            method = "renderStatusBars",
+            at = @At("STORE")
+    )
+    private boolean doNotRenderBlinkingHeartsOnWall(boolean blinking) {
+        return blinking && !SeedQueue.isOnWall();
     }
 }
