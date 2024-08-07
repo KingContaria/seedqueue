efc391890baac5c51da612563fd25b52d55eb2a1
contaria <kingcontaria@gmail.com>
2024-08-07 15:12:58 +0200

    fix: disable sleepbackgrounds ThreadExecutorMixin causes performance issues with seedqueue

--- src/main/java/me/contaria/seedqueue/SeedQueueMixinConfigPlugin.java
+++ src/main/java/me/contaria/seedqueue/SeedQueueMixinConfigPlugin.java
@@ -1,13 +1,16 @@
 package me.contaria.seedqueue;
 
+import com.bawnorton.mixinsquared.api.MixinCanceller;
 import net.fabricmc.loader.api.FabricLoader;
 import org.mcsr.speedrunapi.mixin_plugin.SpeedrunMixinConfigPlugin;
+
+import java.util.List;
 
 /**
  * SeedQueues Mixin Config Plugin, extends SpeedrunAPI's plugin to inherit its functionality.
  * Only used for compatibility with Sodium Mac, will be redundant once Sodium is updated to not need the seperate mac version anymore.
  */
-public class SeedQueueMixinConfigPlugin extends SpeedrunMixinConfigPlugin {
+public class SeedQueueMixinConfigPlugin extends SpeedrunMixinConfigPlugin implements MixinCanceller {
     private static final boolean MAC_SODIUM = FabricLoader.getInstance().isModLoaded("sodiummac");
 
     @Override
@@ -23,5 +26,12 @@ public class SeedQueueMixinConfigPlugin extends SpeedrunMixinConfigPlugin {
             }
         }
         return super.shouldApplyMixin(targetClassName, mixinClassName);
+    }
+
+    @Override
+    public boolean shouldCancel(List<String> targetClassNames, String mixinClassName) {
+        // SleepBackgrounds Thread Executor mixin has been observed to hurt performance when SeedQueue is active
+        // since SeedQueue spawns many more executors
+        return mixinClassName.equals("com.redlimerl.sleepbackground.mixin.MixinThreadExecutor");
     }
 }
