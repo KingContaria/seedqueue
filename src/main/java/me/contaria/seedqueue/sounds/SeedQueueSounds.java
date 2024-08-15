d2b1ba7fdf08d569cbe0d0fb54e4154e418b8eee
contaria <kingcontaria@gmail.com>
2024-08-16 01:34:10 +0200

    fix: revert "metadata attempt" pushed by accident, I suppose it's nice to have it in the git history now

--- src/main/java/me/contaria/seedqueue/sounds/SeedQueueSounds.java
+++ src/main/java/me/contaria/seedqueue/sounds/SeedQueueSounds.java
@@ -1,7 +1,5 @@
 package me.contaria.seedqueue.sounds;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.sound.WeightedSoundSet;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -27,10 +25,5 @@ public class SeedQueueSounds {
 
     private static SoundEvent register(Identifier id) {
         return Registry.register(Registry.SOUND_EVENT, id, new SoundEvent(id));
-    }
-
-    public static <T> T getMetadata(SoundEvent soundEvent) {
-        WeightedSoundSet soundSet = MinecraftClient.getInstance().getSoundManager().get(soundEvent.getId());
-        return soundSet.getSound().
     }
 }
