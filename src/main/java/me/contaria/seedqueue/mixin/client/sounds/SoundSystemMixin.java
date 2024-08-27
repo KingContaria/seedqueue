ab85f496eb5f8bf13cf32aca43070f286fa203d4
contaria <kingcontaria@gmail.com>
2024-08-27 12:18:07 +0200

    fix: crash when trying to play broken sound files closes https://github.com/KingContaria/seedqueue/issues/17

--- src/main/java/me/contaria/seedqueue/mixin/client/sounds/SoundSystemMixin.java
+++ src/main/java/me/contaria/seedqueue/mixin/client/sounds/SoundSystemMixin.java
@@ -1,14 +1,18 @@
 package me.contaria.seedqueue.mixin.client.sounds;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import me.contaria.seedqueue.interfaces.SQSoundSystem;
+import net.minecraft.client.sound.AudioStream;
 import net.minecraft.client.sound.Channel;
 import net.minecraft.client.sound.SoundInstance;
 import net.minecraft.client.sound.SoundSystem;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 @Mixin(SoundSystem.class)
 public abstract class SoundSystemMixin implements SQSoundSystem {
@@ -18,6 +22,26 @@ public abstract class SoundSystemMixin implements SQSoundSystem {
 
     @Shadow
     public abstract void stop(SoundInstance soundInstance);
+
+    @ModifyExpressionValue(
+            method = "play(Lnet/minecraft/client/sound/SoundInstance;)V",
+            at = {
+                    @At(
+                            value = "INVOKE",
+                            target = "Lnet/minecraft/client/sound/SoundLoader;loadStatic(Lnet/minecraft/util/Identifier;)Ljava/util/concurrent/CompletableFuture;"
+                    ),
+                    @At(
+                            value = "INVOKE",
+                            target = "Lnet/minecraft/client/sound/SoundLoader;loadStreamed(Lnet/minecraft/util/Identifier;Z)Ljava/util/concurrent/CompletableFuture;"
+                    )
+            }
+    )
+    private CompletableFuture<AudioStream> crashBrokenSeedQueueSoundFiles(CompletableFuture<AudioStream> audio, SoundInstance soundInstance) {
+        if (soundInstance.getId().getNamespace().equals("seedqueue") && audio.isCompletedExceptionally()) {
+            throw new RuntimeException("Tried to play a broken sound file from a SeedQueue customization pack! If you are using empty sound files to mute sounds on the wall screen, use short silent ones instead.");
+        }
+        return audio;
+    }
 
     // see SoundSystem#closeSounds
     @Override
